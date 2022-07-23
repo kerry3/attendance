d6bf0f44ee8448a97571e70f72b0b96dd1fdaa67
kerry3 <36907312+kerry3@users.noreply.github.com>
2022-07-23 14:32:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,13 @@ url='https://opga100.com/bbs/login.php' #들어가려는 사이트 주소
 
 driver.get(url)  #사이트 접속
 
-print(driver.page_source)
+driver.find_element_by_id('login_id').send_keys('mynamehaha') #id로 찾음, 개발자 도구로 login_id가지고 있다는거 알아내고 그거로 접근. send_key로 아이디 myname haha를 쏴줌 
+driver.find_element_by_id('login_pw').send_keys('123123')
 
-#headless 로해도 똑같음.
+driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div/div/div/div[2]/div[1]/div/div/form/button').click() # 로그인 버튼 클릭. xpath로 접근. 버튼에 우클릭 하고 xpath 찾기 하면됨. click함수로 클릭시킴.
+#1.26 이거 element값 바뀌어서 다시 xpath따서 변경해줌
+
+
+driver.find_element_by_xpath('//*[@id="undefined-sticky-wrapper"]/nav/div[1]/div[1]/div/div/ul/li[7]/a').click() # 커뮤니티 버튼 누르기. 바로 출석체크 창으로 뜸.
+
+driver.find_element_by_xpath('//*[@id="talk_submit"]').click() # 출석체크 버튼 누르기
