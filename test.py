df40b7f046226b998081fdc76074d71791ace788
kerry3 <36907312+kerry3@users.noreply.github.com>
2022-07-23 14:03:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')               # headless
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('--disable-gpu')
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+# 혹은 options.add_argument("--disable-gpu")
 
-driver = webdriver.Chrome(chromedriver, options=chrome_options)
+driver = webdriver.Chrome('chromedriver', chrome_options=options)
+url='https://opga71.com/bbs/login.php' #들어가려는 사이트 주소
+
+#1.26 web driver 버전 바뀌어서 다시 깔아줌. 코드랑 같은 폴더에 있게 하자.
+
+driver.get(url)  #사이트 접속
+
+print(driver.page_source)
+
+#headless 로해도 똑같음.
