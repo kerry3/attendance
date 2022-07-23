29efd63211fd3fe4aca34a8126aaac4a5897563e
kerry3 <36907312+kerry3@users.noreply.github.com>
2022-07-23 14:20:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ options.add_argument("disable-gpu")
 # 혹은 options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome('chromedriver', chrome_options=options)
-url='https://opga71.com/bbs/login.php' #들어가려는 사이트 주소
+url='https://opga100.com/bbs/login.php' #들어가려는 사이트 주소
 
 #1.26 web driver 버전 바뀌어서 다시 깔아줌. 코드랑 같은 폴더에 있게 하자.
 
